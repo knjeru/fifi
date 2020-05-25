421645a5821b42b7f2187892cc295040e02b1489
Kevin Njeru <kevin@Kevins-MacBook-Pro.local>
2020-05-25 16:20:31 -0400

    grab DB config from env

--- deno-server/app/config/db.config.ts
+++ deno-server/app/config/db.config.ts
@@ -1,14 +1,20 @@
 import Dex from "https://deno.land/x/dex/mod.ts";
 import { Client } from "https://deno.land/x/postgres/mod.ts";
 
+const env = Deno.env;
+
+const DB_HOST = env.get('POSTGRES_HOST') || "localhost";
+const DB_USER = env.get('POSTGRES_USER') || "kevin";
+const DB_PASSWORD = env.get('POSTGRES_PASSWORD') || "";
+const DB_NAME = env.get('POSTGRES_DB') || "fifi";
 
 export const dex = new Dex({
     client: "postgres"
 });
 
 export const dbClient = new Client({
-    password: "postgres",
-    user: "postgres",
-    hostname: "postgres",
-    database: "fifi"
+    password: DB_PASSWORD,
+    user: DB_USER,
+    hostname: DB_HOST,
+    database: DB_NAME
 });
